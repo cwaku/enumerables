fec496787e80d2fe8103acc11adcce36489f3dd5
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-03 16:06:59 +0100

    Add enumerable methods

---
+++ Enumerable.rb
@@ -0,0 +1,21 @@
+module Enumerable
+  def all?(&block)
+    result = true 
+    each do |element| result = false
+      unless block.call(element)
+    result
+  end
+end
+
+  def any?(&block)
+    result = false
+      each do |element| result = true 
+      if block.call(element)
+    result
+  end
+
+  def filter?(&block)
+    # Returns elements selected by the block.
+    self.select { |item| block.call(item) }
+  end
+end
--- my_list.rb
+++ my_list.rb
@@ -1,6 +1,9 @@
+require_relative './Enumerable'
+
 # Create a class MyList that has an instance variable @list.
 class MyList
-  def initialize(list)
+  include Enumerable
+  def initialize(*list)
     @list = list
   end
 
@@ -9,3 +12,22 @@ class MyList
     @list.each(&block)
   end
 end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+list.all? { |e| e < 5 }
+# => true
+list.all? { |e| e > 5 }
+# => false
+
+# Test #any?
+list.any? { |e| e == 2 }
+# => true
+list.any? { |e| e == 5 }
+# => false
+
+# Test #filter
+list.filter { |e| e.even? }
+# => [2, 4]
