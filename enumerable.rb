4ff518ac50abfbc015bbd1afbc84743f2d29f159
Ricky Mormor <rickymormor1@gmail.com>
2022-03-03 14:42:11 +0000

    Add enumerables

--- enumerable.rb
+++ enumerable.rb
@@ -1,10 +1,22 @@
 module enumerable
-  def all?
-    result = true
-      each {}
-      
-      end
-   
-    max
+  def all?(&block)
+    return true if self.empty?
+    self.each do |item|
+      return false unless block.call(item)
+    end
+    true
   end
+
+  def any?(&block)
+    return true if self.empty?
+    self.each do |item|
+      return true if block.call(item)
+    end
+    false
+  end
+
+  def filter? (&block)
+    self.select {|item| block.call(item)}
+  end
+
 end
